71035d94c2cbc847e9410e16f8263f2d5e1c3ce2
G-Nicacio <gustavo.aydelkop@gmail.com>
2025-11-10 10:57:51 -0300

    ok achei o erro agr foi

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://18.228.40.174:8080',
         changeOrigin: true,
       },
     },
