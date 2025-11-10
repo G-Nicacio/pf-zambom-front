4869c6672ad704bb5d99b5db462a1ed4f9361248
G-Nicacio <gustavo.aydelkop@gmail.com>
2025-11-10 09:00:57 -0300

    agr ta certo

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
-      clientId="4rwe12yYJbxAD122EDkjl61unHBaHFL0"
+      domain="dev-gfdpj05ngeziykup.us.auth0.com"
+      clientId="OKRG2SK4ZWwrmoLSfdjpam6Df52BX4JJ"
       authorizationParams={{
-        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        audience: "https://dev-gfdpj05ngeziykup.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
